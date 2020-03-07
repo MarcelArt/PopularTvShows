a735181415f787228357230a1ef4ec9ba7e690c3
MarcelArt <marcel.aritonang@gmail.com>
2020-03-07 22:41:43 +0700

    comment + hapus code tak terpakai

--- app/src/main/java/com/example/populartvshows/MainActivity.java
+++ app/src/main/java/com/example/populartvshows/MainActivity.java
@@ -36,37 +36,43 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //hubungin view2nya ke layout
         rv_tv = (RecyclerView) findViewById(R.id.rv_tvs);
         spin_tv = (Spinner) findViewById(R.id.spin_tv);
         rv_tv.setHasFixedSize(true);
 
+        //configurasi layout recycler view
         layoutManager = new LinearLayoutManager(this);
         rv_tv.setLayoutManager(layoutManager);
 
-
-//        tvAdapter = new
-
+        //ambil data dari themoviedb
         fetchTvs();
 
+        //event listener untuk ketika item spinner dipilih
         spin_tv.setOnItemSelectedListener(this);
     }
 
     public void fetchTvs() {
+        //membuat koneksi ke themoviedb mungkin lebih bagus jika dibuat kelas statik agar dapat didaur ulang untuk halaman lainnya
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.themoviedb.org/3/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
+        //panggil api
         APIService service = retrofit.create(APIService.class);
-
         Call<Tvs> call = service.listTvs();
         call.enqueue(new Callback<Tvs>() {
             @Override
             public void onResponse(Call<Tvs> call, Response<Tvs> response) {
-                tvs = response.body();
+                tvs = response.body(); //simpan response api ke variabel
+
+                //menyimpan hasil response ke recylerview
                 tvAdapter = new TVAdapter(tvs.getResults());
                 rv_tv.setAdapter(tvAdapter);
 
+                //simpan hasil response ke spinner
                 ArrayAdapter<Tv> spinnerAdapter = new ArrayAdapter<Tv>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, tvs.getResults());
                 spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 spin_tv.setAdapter(spinnerAdapter);
@@ -77,23 +83,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 Log.d("data", "error: " + t.getMessage());
             }
         });
-
-//        call.enqueue(new Callback() {
-//            @Override
-//            public void onResponse(Call<Tvs> call, Response response) {
-//                Log.d("data", "data: " + response.body().toString());
-//                tvs = response.body().
-////                tvAdapter = new TVAdapter(response.body());
-//            }
-//
-//            @Override
-//            public void onFailure(Call<Tvs> call, Throwable t) {
-//                Log.d("data", "error: " + t.getMessage());
-//            }
-//        });
-
-
-//        Log.d("data", "data" + tvs);
     }
 
     @Override
